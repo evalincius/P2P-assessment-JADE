66096186ab4a87dddd7ee39b29226aadf5462b4f
evalincius <ed@oa-edu-169-116.wireless.abdn.ac.uk>
2014-11-11 21:08:02 +0000

    P2P JADE ASSESMENT
    created agents on a runtime

---
+++ src/jade/Simulation.java
@@ -0,0 +1,44 @@
+package jade;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+import jade.core.*; 
+import jade.core.Runtime;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.util.Logger;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.ControllerException;
+
+public class Simulation extends Agent {
+	private Logger myLogger = Logger.getMyLogger(getClass().getName());
+	
+	
+	// get a JADE runtime
+	
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = -3200493830320510548L;{
+	try {
+		jade.core.Runtime runtime = jade.core.Runtime.instance();
+		//Creates new container
+		jade.wrapper.AgentContainer home = runtime.createAgentContainer(new ProfileImpl());
+		// Adds new agent to container
+		AgentController a = home.createNewAgent("a2",HostCache.class.getName(), new Object[0]);
+		a.start();
+		//home.createNewAgent("2", "jade.HostCache", null);
+		} catch (jade.wrapper.StaleProxyException e) {
+		System.err.println("Error launching agent...");
+		}
+	}
+	
+	
+	
+}
+
